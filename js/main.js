0019ca46f7314a90af0c0a7a0e22fef45edfcca7
Victor Romanov <romanovvictor1985@gmail.com>
2018-04-06 21:46:04 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+	$(".toggleMobMenu").on("click",function(){
+		$(".menuCont").stop().slideToggle();
+	});
+
+	$(".searchIcon").on("click",function(){
+		$(".header__form").stop().slideToggle();
+	});
+
+});
+
+$(function(){
+    $("#sort").niceSelect();
+});
