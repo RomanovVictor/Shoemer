0efc92bb45ac85cc78002928a7cf924dae67337d
Victor Romanov <romanovvictor1985@gmail.com>
2018-04-10 22:23:41 +0300

    task complete

--- js/main.js
+++ js/main.js
@@ -12,3 +12,31 @@ $(function(){
 $(function(){
     $("#sort").niceSelect();
 });
+
+
+
+$(function(){
+	$(".toggleMobMenu").on("click",function(){
+		$(".menu__bottom").stop().slideToggle();
+	});
+
+	$(".searchIcon").on("click",function(){
+		$(".header__form").stop().slideToggle();
+	});
+
+});
+
+
+$(function(){
+    $(".accordion__itemTitle").on("click",function(){
+        
+        var accordionItem = $(this).closest(".accordion__item");
+        var accordionItemSiblings = $(accordionItem).siblings(".accordion__item");
+        var accordionCheckboxWrap = $(accordionItemSiblings).children(".accordion__checkboxWrap");
+        
+        $(accordionCheckboxWrap).hide();
+        
+        $(this).siblings(".accordion__checkboxWrap").slideToggle();
+    
+    });
+});
