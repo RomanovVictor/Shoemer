46eb8b901f9e16e19f5d754cced5baf182322b50
Victor Romanov <romanovvictor1985@gmail.com>
2018-04-16 21:02:15 +0300

    last commit

--- js/main.js
+++ js/main.js
@@ -1,31 +1,39 @@
-$(function(){
-	$(".toggleMobMenu").on("click",function(){
-		$(".menuCont").stop().slideToggle();
-	});
 
-	$(".searchIcon").on("click",function(){
-		$(".header__form").stop().slideToggle();
-	});
-
-});
+/*------------------SELECT--------------------*/
 
 $(function(){
     $("#sort").niceSelect();
 });
 
-
+/*----------------DROPDOWN MENU---------------*/
 
 $(function(){
 	$(".toggleMobMenu").on("click",function(){
 		$(".menu__bottom").stop().slideToggle();
 	});
+    
 
 	$(".searchIcon").on("click",function(){
 		$(".header__form").stop().slideToggle();
 	});
+      
+    
+    $(".toggleMobMenu").on("click",function(){
+		$(".menu__top").stop().slideToggle();
+	});
+    
 
+	$(".toggleMobMenu").on("click",function(){
+		$(".header_bottom_basket").stop().slideToggle();
+	});
+    
+
+	$(".searchIcon").on("click",function(){
+		$(".header__form").stop().slideToggle();
+	});
 });
 
+/*-------------------ACCORDION------------------*/
 
 $(function(){
     $(".accordion__itemTitle").on("click",function(){
@@ -38,5 +46,18 @@ $(function(){
         
         $(this).siblings(".accordion__checkboxWrap").slideToggle();
     
+    });
+});
+
+/*------------------MODAL WINDOW------------------*/
+
+$(function(){
+    $(".openModalBtn").on("click",function(){
+        $(".modal").addClass("active");
+    });
+        
+    $(".closeModalBtn").on("click",function(){
+        $(".modal").removeClass("active");
+        
     });
 });
